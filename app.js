6335a57d2c18295cc13741e46f8f43fa1c81e9ac
patrick_mugo <patrickkinyua2004@gmail.com>
2026-02-02 22:20:41 +0300

    render dep prep

--- app.js
+++ app.js
@@ -38,7 +38,9 @@ app.use((err, req, res, next) => {
     })
 })
 
-app.listen(5001, () => {
-    console.log("Server is running on http://localhost:5001");
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
     connectDb();    
 });
